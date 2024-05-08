ad26a0eb7536f1264067cd99a5d10325134cf079
debleena <debleena.btkol@gmail.com>
2024-05-08 11:58:57 +0530

    locomotive smooth scroll replaced by scrolltrigger smmothscroll, splideJS added

--- js/custom.js
+++ js/custom.js
@@ -26,63 +26,77 @@ window.onscroll=function(){
 
 gsap.registerPlugin(ScrollTrigger);
 
-const pageContainer = document.querySelector(".scroll-container");
-pageContainer.setAttribute("data-scroll-container", "");
 
-const scroller = new LocomotiveScroll({
-  el: pageContainer,
-  smooth: true,
-  getDirection: true
-});
+const container = document.querySelector(".scroll-container")
 
-scroller.on("scroll", function (t) {
-  document.documentElement.setAttribute("data-direction", t.direction);
-});
+let height
+function setHeight() {
+  height = container.clientHeight
+  document.body.style.height = height + "px"
+}
+ScrollTrigger.addEventListener("refreshInit", setHeight)
+
+// smooth scrolling container
+gsap.to(container, {
+  y: () => -(height - document.documentElement.clientHeight),
+  ease: "none",
+  scrollTrigger: {
+    trigger: document.body,
+    start: "top top",
+    end: "bottom bottom",
+    scrub: 0.5,
+    invalidateOnRefresh: true
+  }
+})
+
+const sections = gsap.utils.toArray('.appear-down')
+
+sections.forEach(function (sec, i) {	
+  const y = sec.offsetTop - 100
+  gsap.fromTo(sec, {
+    y: 100,
+    opacity: 0,
+    scale: 0.9,
+  }, {
+    y: 0,
+    opacity: 1,
+    scale: 1,
+    scrollTrigger: {
+      trigger: sec,
+      scrub: 1,
+      start: 'top bottom',
+      end: '+=200',
+    }
+  })
+})
 
 
 
-ScrollTrigger.scrollerProxy(pageContainer, {
-        scrollTop(value) {
-            return arguments.length ?
-            scroller.scrollTo(value, 0, 0) :
-            scroller.scroll.instance.scroll.y;
-        },
-        getBoundingClientRect() {
-            return {
-                left: 0, 
-                top: 0, 
-                width: window.innerWidth,
-                height: window.innerHeight
-            };
-        },
-        // pinType: pageContainer.style.transform ? "transform" : "fixed"
-    });
-
-    ScrollTrigger.matchMedia({
+// ScrollTrigger.matchMedia({
       
-      "(min-width: 200px)": function() {
-        // gsap.registerPlugin(ScrollTrigger);
-      
-          let slideSections = gsap.utils.toArray(".latest-track__slider--model__card");
-      
-          gsap.to(slideSections, {
-              xPercent: -100 * (slideSections.length - 1),
-              ease: "none",
-              scrollTrigger: {
-                  scroller: "[data-scroll-container]",
-                  trigger: ".latest-track__slider",
-                  start: "top",
-                  pin: true,
-                  scrub: 1,
-                  // snap: 1 / (sections.length - 1),
-                  end: () => "+=" + document.querySelector(".latest-track__slider").offsetWidth,
-                  // invalidateOnRefresh: true
-              }
-          }); 
-      },
-      });
+//         "(min-width: 200px)": function() {
+//           gsap.registerPlugin(ScrollTrigger);
+        
+            // let slideSections = gsap.utils.toArray(".latest-track__slider--model__card");
+        
+            // gsap.to(slideSections, {
+            //     xPercent: -100 * (slideSections.length - 1),
+            //     ease: "none",
+            //     scrollTrigger: {
+            //         // scroller: "[data-scroll-container]",
+            //         trigger: ".latest-track__slider",
+            //         start: "top",
+            //         pin: true,
+            //         scrub: true,
+            //         snap: 1 / (sections.length - 1),
+            //         end: () => "+=" + document.querySelector(".latest-track__slider").offsetWidth,
+            //         // invalidateOnRefresh: true
+            //     }
+            // }); 
+        // },
+        // });
 
-      const mediaQuery = window.matchMedia('(min-width: 200px)')
+const mediaQuery = window.matchMedia('(min-width: 200px)')
       if (mediaQuery.matches) {
       const bgProgress = document.querySelector('.pre-footer__overlay');
       const goalBlock = document.querySelector('.pre-footer'); 
@@ -101,11 +115,136 @@ ScrollTrigger.scrollerProxy(pageContainer, {
           bgProgress.style.opacity = '0';
         }
       };
-      // window.addEventListener('scroll', scrollBgProgress);
-      scroller.on('scroll', scrollBgProgress);
-      }
+      window.addEventListener('scroll', scrollBgProgress);
+      // scroller.on('scroll', scrollBgProgress);
+    }
+
+    document.addEventListener("mousemove", parallax);
+      function parallax(e) {
+        document
+          .querySelectorAll(".banner__object--image").forEach(function (move) {
+            var movingValue = move.getAttribute("data-value");
+            var x = (e.clientX * movingValue) / 150;
+            var y = (e.clientY * movingValue) / 150;
+
+            move.style.transform =
+              "translateX(" + x + "px) translateY(" + y + "px)";
+          });
+      };
+ 
+
+      new Splide('.track-splide', {
+      type: 'loop',
+      drag: 'free',
+      // focus: 'center',
+      perPage: 1,
+      // padding: '50px',
+      gap: '50px',
+      pagination: false,
+      // arrows: false,
+      autoScroll: {
+        speed: 0.7,
+      },
+      breakpoints: {
+        1400: {
+          gap: '40px',
+        },
+        1200: {
+          gap: '35px',
+        },
+        1024: {
+          gap: '30px',
+        },
+        767: {
+          gap: '20px',
+        },
+        600: {
+          gap: '15px',
+        },
+      },
+    }).mount(window.splide.Extensions);
+
+
+// const pageContainer = document.querySelector(".scroll-container");
+// pageContainer.setAttribute("data-scroll-container", "");
+
+// const scroller = new LocomotiveScroll({
+//   el: pageContainer,
+//   smooth: true,
+//   getDirection: true
+// });
+
+// scroller.on("scroll", function (t) {
+//   document.documentElement.setAttribute("data-direction", t.direction);
+// });
+
+
+
+// ScrollTrigger.scrollerProxy(pageContainer, {
+//         scrollTop(value) {
+//             return arguments.length ?
+//             scroller.scrollTo(value, 0, 0) :
+//             scroller.scroll.instance.scroll.y;
+//         },
+//         getBoundingClientRect() {
+//             return {
+//                 left: 0, 
+//                 top: 0, 
+//                 width: window.innerWidth,
+//                 height: window.innerHeight
+//             };
+//         },
+//         // pinType: pageContainer.style.transform ? "transform" : "fixed"
+//     });
+
+//     ScrollTrigger.matchMedia({
+      
+//       "(min-width: 200px)": function() {
+//         // gsap.registerPlugin(ScrollTrigger);
+      
+//           let slideSections = gsap.utils.toArray(".latest-track__slider--model__card");
+      
+//           gsap.to(slideSections, {
+//               xPercent: -100 * (slideSections.length - 1),
+//               ease: "none",
+//               scrollTrigger: {
+//                   scroller: "[data-scroll-container]",
+//                   trigger: ".latest-track__slider",
+//                   start: "top",
+//                   pin: true,
+//                   scrub: 1,
+//                   // snap: 1 / (sections.length - 1),
+//                   end: () => "+=" + document.querySelector(".latest-track__slider").offsetWidth,
+//                   // invalidateOnRefresh: true
+//               }
+//           }); 
+//       },
+//       });
+
+//       const mediaQuery = window.matchMedia('(min-width: 200px)')
+//       if (mediaQuery.matches) {
+//       const bgProgress = document.querySelector('.pre-footer__overlay');
+//       const goalBlock = document.querySelector('.pre-footer'); 
+    
+//       const scrollBgProgress = () => {
+        
+//         let scrollBgDistance = ((goalBlock.getBoundingClientRect().top) + 100);
+//         let bgProgressPercentage =
+//           (scrollBgDistance /
+//             (goalBlock.getBoundingClientRect().height - document.documentElement.clientHeight)) * 100;
+      
+//         let val = Math.floor(bgProgressPercentage);
+//         bgProgress.style.opacity = val;
+      
+//         if (val < 0) {
+//           bgProgress.style.opacity = '0';
+//         }
+//       };
+//       // window.addEventListener('scroll', scrollBgProgress);
+//       scroller.on('scroll', scrollBgProgress);
+//       }
   
-      scroller.on('scroll', ScrollTrigger.update);
-      ScrollTrigger.addEventListener('refresh', () => scroller.update());
-      ScrollTrigger.refresh();
+//       scroller.on('scroll', ScrollTrigger.update);
+//       ScrollTrigger.addEventListener('refresh', () => scroller.update());
+//       ScrollTrigger.refresh();
 
